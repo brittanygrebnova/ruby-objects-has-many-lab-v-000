603f808a2845a70cd8c9c456a4d248776d915697
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-04-09 18:44:20 +0000

    automatically backed up by learn

---
+++ lib/post.rb
@@ -0,0 +1,16 @@
+class Post
+  
+  attr_accessor :author, :title
+  
+  def initialize(title)
+    @author = author
+    @title = title
+  end
+  
+  def author_name
+    if self.author = true
+      self.author.name
+    end
+  end
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+class Song
+  
+  attr_accessor :artist, :name
+  
+  def initialize(name)
+    @artist = artist
+    @name = name
+  end
+  
+end
